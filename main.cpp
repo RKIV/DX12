88ab35c4a9967d3423df96bd67f66978b379f2b0
Robert Keller <robertkellerIV@gmail.com>
2019-06-20 01:08:41 -0400

    Initialize README, create main.cpp with basic includes, add d3dx12.h helper file.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#define WIND32_LEAN_AND_MEAN
+#include <Windows.H>
+#include <shellapi.h> // For CommandLineToArgvW
+
+// The min/max macros conflict with like-named memeber functions
+// Only use std::min and std::max defined in <algorithm>.
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+// In order to define a function called CreateWindow, the Window macro needs to 
+// be undefined.
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+//  Window Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+#include <d3dx12.h>
+
+// STL Headers
+#include <algorithm>
+#include <cassert>
+#include <chrono>
